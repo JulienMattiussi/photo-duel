ff62f71417ee17766439401c924f5d85c33a6b59
JulienM <julienm@marmelab.com>
2023-10-27 18:22:27 +0200

    fix ts

--- src/main.ts
+++ src/main.ts
@@ -10,29 +10,36 @@ interface DuelRouteParams {
   title: string;
 }
 
-function WebmailViewModel() {
+interface WebmailViewModel {
+  duels: Duel[];
+  chosenDuel: ko.Observable<Duel | undefined>;
+  goToDuel: (duel: Duel) => void;
+}
+
+class WebmailViewModel {
   // Data
-  var self = this;
-  self.duels = duels.sort((a, b) => (a.date > b.date ? 1 : -1));
-  self.chosenDuel = ko.observable();
+  constructor() {
+    this.duels = duels.sort((a, b) => (a.date > b.date ? 1 : -1));
+    this.chosenDuel = ko.observable();
 
-  // Behaviours
-  self.goToDuel = function (duel: Duel) {
-    router.navigate(duel.title);
-  };
+    // Behaviours
+    this.goToDuel = function (duel: Duel) {
+      router.navigate(duel.title);
+    };
 
-  var router = new Navigo("/", { hash: true });
+    var router = new Navigo("/", { hash: true });
 
-  router.on("/:title", (params) => {
-    const { title } = params?.data as DuelRouteParams;
-    self.chosenDuel(duels.find((duel) => duel.title === title));
-  });
+    router.on("/:title", (params) => {
+      const { title } = params?.data as DuelRouteParams;
+      this.chosenDuel(duels.find((duel) => duel.title === title));
+    });
 
-  router.on("/", () => {
-    router.navigate(`/${self.duels[0].title}`);
-  });
+    router.on("/", () => {
+      router.navigate(`/${this.duels[0].title}`);
+    });
 
-  router.resolve("/");
+    router.resolve("/");
+  }
 }
 
 ko.applyBindings(new WebmailViewModel());
