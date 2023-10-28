2803d29e8db0410ea6cab41c8a9c4a58a99f8608
JulienM <julienm@marmelab.com>
2023-10-28 21:38:30 +0200

    fix data type

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,6 @@ import ko from "knockout";
 import Navigo from "navigo";
 import duels, { Duel } from "../public/images/index.ts";
 
-console.log(duels);
-
 interface DuelRouteParams {
   [key: string]: string;
   title: string;
@@ -14,6 +12,8 @@ interface WebmailViewModel {
   duels: Duel[];
   chosenDuel: ko.Observable<Duel | undefined>;
   goToDuel: (duel: Duel) => void;
+  isImage: (filename: string) => boolean;
+  isVideo: (filename: string) => boolean;
 }
 
 class WebmailViewModel {
@@ -27,7 +27,7 @@ class WebmailViewModel {
       router.navigate(duel.title);
     };
 
-    var router = new Navigo("/", { hash: true });
+    let router = new Navigo("/", { hash: true });
 
     router.on("/:title", (params) => {
       const { title } = params?.data as DuelRouteParams;
@@ -40,6 +40,15 @@ class WebmailViewModel {
 
     router.resolve("/");
   }
+
+  //Functions
+  isImage = (filename: string) => {
+    return !!filename.match(/\.(jpg|jpeg|png|gif)$/i);
+  };
+
+  isVideo = (filename: string) => {
+    return !!filename.match(/\.(mp4|webm|ogg)$/i);
+  };
 }
 
 ko.applyBindings(new WebmailViewModel());
